919b7a12988041a5739cdbf2bb00b1b55d06fb7d
Max Tarlov <maxtarlov@gmail.com>
2022-10-30 16:51:42 -0700

    disable and re-enable inputs while waiting on gcp

--- assets/js/bot-interface.js
+++ assets/js/bot-interface.js
@@ -1,38 +1,41 @@
 // Javascript for bot interface
 
 window.addEventListener("load", () => {
+    
+    function handleResponse(data) {
+        form['user-question'].value = data['user-question'];
+        responseArea.textContent = data['bot-answer'];
+        form['user-question'].disabled = false;
+        submitButton.disabled = false;
+        console.log("Cloud function response:");
+        console.log(data);
+    }
+
     function warmUpCloudFunction() {
         console.log("Warming up cloud function");
         fetch("https://handle-question-irwuk7yqaq-uc.a.run.app/")
             .then((response) => response.json())
-            .then((data) => {
-                form['user-question'].value = data['user-question'];
-                responseArea.textContent = data['bot-answer'];
-                submitButton.disabled = false;
-                console.log("Cloud function response:");
-                console.log(data);
-            });
+            .then(handleResponse);
     }
+
     function sendData() {
-        console.log(form['user-question'].value);
+        responseArea.textContent = "Loading...";
         console.log("Sending user question");
         fetch(`https://handle-question-irwuk7yqaq-uc.a.run.app/?q=${encodeURIComponent(form['user-question'].value)}`)
             .then((response) => response.json())
-            .then((data) => {
-                form['user-question'].value = data['user-question'];
-                responseArea.textContent = data['bot-answer'];
-                submitButton.disabled = false;
-                console.log("Cloud function response:");
-                console.log(data);
-            });
+            .then(handleResponse);
     }
 
     const form = document.getElementById("bot-ui-form");
     const submitButton = document.getElementById("submit-button");
     const responseArea = document.getElementById("response-text");
+
     warmUpCloudFunction();
+
     form.addEventListener("submit", (event) => {
         event.preventDefault();
+        form['user-question'].disabled = true;
+        submitButton.disabled = true;
         sendData();
     });
 });
