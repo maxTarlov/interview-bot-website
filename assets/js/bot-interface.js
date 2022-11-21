e9c1e780ec84f4d0dd6b31ec60363f68bd4e9d15
Max Tarlov <maxtarlov@gmail.com>
2022-11-20 16:49:00 -0800

    Added feedback buttons to bot-ui

--- assets/js/bot-interface.js
+++ assets/js/bot-interface.js
@@ -2,13 +2,57 @@
 
 window.addEventListener("load", () => {
 
-    const backEndURL = "https://handle-question-irwuk7yqaq-uw.a.run.app/";
+    function feedbackHandlerFactory(logID, good) {
+        function result() {
+            if(good) {
+                feedbackPositive.disabled = true;
+                feedbackNegative.disabled = false;
+            }
+            else {
+                feedbackPositive.disabled = false;
+                feedbackNegative.disabled = true;
+            }
+
+            const feedbackData = {
+                "id": logID,
+                "good": good
+            }
+            
+            fetch(backEndURL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(feedbackData),
+                })
+                .then((response) => {
+                    console.log(`Feedback status: ${response.status}`)
+                    console.log(response)
+                });
+        }
+        
+        return result
+    }
+
+    function enableFeedbackButtons(logID) {
+        feedbackPositive.disabled = false;
+        feedbackNegative.disabled = false;
+
+        feedbackPositive.addEventListener("click", feedbackHandlerFactory(logID, true));
+        feedbackNegative.addEventListener("click", feedbackHandlerFactory(logID, false));   
+    }
+
+    function disableFeedbackButtons() {
+        feedbackPositive.disabled = true;
+        feedbackNegative.disabled = true;
+    }
     
     function handleResponse(data) {
         form['user-question'].value = data['user-question'];
         responseArea.textContent = data['bot-answer'];
         form['user-question'].disabled = false;
         submitButton.disabled = false;
+        enableFeedbackButtons(data['id']);
         console.log("Cloud function response:");
         console.log(data);
     }
@@ -28,9 +72,14 @@ window.addEventListener("load", () => {
             .then(handleResponse);
     }
 
+    const backEndURL = "https://handle-question-irwuk7yqaq-uw.a.run.app/";
+
     const form = document.getElementById("bot-ui-form");
     const submitButton = document.getElementById("submit-button");
     const responseArea = document.getElementById("response-text");
+
+    const feedbackPositive = document.getElementById("feedback-positive");
+    const feedbackNegative = document.getElementById("feedback-negative");
 
     warmUpCloudFunction();
 
@@ -38,6 +87,7 @@ window.addEventListener("load", () => {
         event.preventDefault();
         form['user-question'].disabled = true;
         submitButton.disabled = true;
+        disableFeedbackButtons();
         sendData();
     });
 });
